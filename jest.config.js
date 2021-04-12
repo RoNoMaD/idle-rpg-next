c70936642605ad2836f935b1efed09a7ef22f4b8
Romain Lebascle <romain.lebascle@gmail.com>
2021-04-12 09:47:48 +0200

    feat: add module name mapper to jest to be the same as in tsconfig

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   roots: ["<rootDir>"],
   moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
   testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
+  setupFiles: [require.resolve("whatwg-fetch")],
   transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
   transform: {
     "^.+\\.(ts|tsx)$": "babel-jest",
@@ -11,6 +12,11 @@ module.exports = {
     "jest-watch-typeahead/testname",
   ],
   moduleNameMapper: {
+    "@api-client/(.*)": "<rootDir>/api-client/$1",
+    "@components/(.*)": "<rootDir>/components/$1",
+    "@dao/(.*)": "<rootDir>/dao/$1",
+    "@lib/(.*)": "<rootDir>/lib/$1",
+    "@services/(.*)": "<rootDir>/services/$1",
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
     "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
   },
